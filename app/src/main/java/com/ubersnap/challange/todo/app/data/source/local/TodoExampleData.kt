50d0352c11543efa8d50e392608d5f90b4080f8e
Irshal Firdeansyah <irshalfir@gmail.com>
2023-07-26 00:24:26 +0700

    ui: theme: update app color from generated material color

--- app/src/main/java/com/ubersnap/challange/todo/app/data/source/local/TodoExampleData.kt
+++ app/src/main/java/com/ubersnap/challange/todo/app/data/source/local/TodoExampleData.kt
@@ -6,16 +6,124 @@ object TodoExampleData {
 
     val todos = listOf(
         TodoEntity(
-            id = 1,
+            id = 1L,
             title = "Disk Clean up",
             description = "Clean up my friend disk on their house",
             dueDate = 1690282564000
         ),
         TodoEntity(
-            id = 2,
+            id = 2L,
             title = "Finish code challenge",
             description = "Finish code challange from Ubersnap",
             dueDate = 1690279200000
+        ),
+        TodoEntity(
+            id = 3L,
+            title = "Call John",
+            description = "Discuss upcoming project details",
+            dueDate = 1690452000000L
+        ),
+        TodoEntity(
+            id = 4L,
+            title = "Team meeting",
+            description = "Weekly team meeting at the office",
+            dueDate = 1690538400000L
+        ),
+        TodoEntity(
+            id = 5L,
+            title = "Plan summer vacation",
+            description = "Book flight tickets and accommodation",
+            dueDate = 1690624800000L
+        ),
+        TodoEntity(
+            id = 6L,
+            title = "Go for a jog",
+            description = "Exercise in the park",
+            dueDate = 1690711200000L
+        ),
+        TodoEntity(
+            id = 7L,
+            title = "Pay bills",
+            description = "Electricity and internet bills",
+            dueDate = 1690797600000L
+        ),
+        TodoEntity(
+            id = 8L,
+            title = "Read book",
+            description = "Finish 'The Great Gatsby'",
+            dueDate = 1690884000000L
+        ),
+        TodoEntity(
+            id = 9L,
+            title = "Prepare presentation",
+            description = "Slides for the client meeting",
+            dueDate = 1690970400000L
+        ),
+        TodoEntity(
+            id = 10L,
+            title = "Clean the house",
+            description = "Vacuum and dust all rooms",
+            dueDate = 1691056800000L
+        ),
+        TodoEntity(
+            id = 11L,
+            title = "Call mom",
+            description = "Wish her a happy birthday",
+            dueDate = 1691143200000L
+        ),
+        TodoEntity(
+            id = 12L,
+            title = "Write blog post",
+            description = "Topic: Benefits of meditation",
+            dueDate = 1691229600000L
+        ),
+        TodoEntity(
+            id = 13L,
+            title = "Renew gym membership",
+            description = "One-year subscription",
+            dueDate = 1691316000000L
+        ),
+        TodoEntity(
+            id = 14L,
+            title = "Fix leaky faucet",
+            description = "In the kitchen sink",
+            dueDate = 1691402400000L
+        ),
+        TodoEntity(
+            id = 15L,
+            title = "Submit expense report",
+            description = "For the business trip",
+            dueDate = 1691488800000L
+        ),
+        TodoEntity(
+            id = 16L,
+            title = "Buy a gift",
+            description = "Friend's wedding anniversary",
+            dueDate = 1691575200000L
+        ),
+        TodoEntity(
+            id = 17L,
+            title = "Study for exam",
+            description = "Mathematics and physics",
+            dueDate = 1691661600000L
+        ),
+        TodoEntity(
+            id = 18L,
+            title = "Paint the living room",
+            description = "Choose a new color",
+            dueDate = 1691748000000L
+        ),
+        TodoEntity(
+            id = 19L,
+            title = "Plan weekend trip",
+            description = "Explore nearby attractions",
+            dueDate = 1691834400000L
+        ),
+        TodoEntity(
+            id = 20L,
+            title = "Call the plumber",
+            description = "Fix the clogged drain",
+            dueDate = 1691920800000L
         )
     )
 
